b57e25d8d0f6bf73904df0345407fa92da7107e8
alexandrekermarec <alexkermarec@hotmail.fr>
2024-12-12 20:00:54 +0100

    pt token addr fix

--- frontend/app/dapp/strategyone/page.tsx
+++ frontend/app/dapp/strategyone/page.tsx
@@ -10,7 +10,7 @@ import { useTransactionToast } from "@/hooks/use-transaction-toast";
 import Link from "next/link";
 import { ArrowLeft, BookText } from "lucide-react";
 
-const PT_TOKEN_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const PT_TOKEN_ADDRESS = "0xd5F274E114b72E7DA9a92e89885E79EA030dc112";
 
 const StrategyOnePage = () => {
     // Récupération du role
